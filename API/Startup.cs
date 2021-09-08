85a62e460a5e3b004972da1e17efe844e8421cff
wilsonmichaelunl <wilson.michaelthomas@gmail.com>
2021-09-08 17:39:15 -0500

    end of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,14 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
             services.AddApplicationServices();
-            services.AddSwaggerDocumentation();           
+            services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
